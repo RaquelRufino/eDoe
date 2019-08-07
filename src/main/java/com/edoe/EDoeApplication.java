6b8a3f90e3563818ea8f3b77bc8e0127b3c72cfb
RaquelRufino <quelrufino35@gmail.com>
2019-08-07 14:37:04 -0300

    add initial project

---
+++ src/main/java/com/edoe/EDoeApplication.java
@@ -0,0 +1,13 @@
+package com.edoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDoeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edoe/EDoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EDoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
