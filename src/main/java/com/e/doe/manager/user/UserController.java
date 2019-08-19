c026f9a67cb21bb97aa679e1d5b0ef86afd4bd1c
Raquel Paz <raquel.paz@ext.tfgco.com>
2019-08-19 11:46:16 -0300

    add User Service

--- src/main/java/com/e/doe/manager/user/UserController.java
+++ src/main/java/com/e/doe/manager/user/UserController.java
@@ -2,9 +2,7 @@ package com.e.doe.manager.user;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,66 +29,53 @@ import javax.validation.Valid;
 @Api(tags = "User")
 public class UserController {
 	
+	private UserService userService;
+
 	@Autowired
-	UserRepository userRepository;
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+	
 	
 	@ApiOperation(value="Get All Users")
 	@GetMapping({"/", ""})
 	public List<User> getUsers(){
-		return userRepository.findAll();
+		return userService.getUsers();
 	}
 	
 	@ApiOperation(value="Get a User")
 	@GetMapping({"/{id}/", "/{id}"})
 	public User getUserById(@PathVariable(value="id") String id){
-		return userRepository.findById(id);
+		return userService.getUserById(id);
 	}
 	
 	@ApiOperation(value="Get a User by name")
 	@GetMapping({"/{name}/", "/{name}"})
 	public String getUsuariosByName(@PathVariable(value="name") String name){
 
-		List<User> users = userRepository.findByName(name);
-		String st = "";
-		
-		for (int i = 0; i < users.size() - 1; i++) {
-			st += users.get(i).toString() + " | ";
-		}
-
-		st += users.get(users.size() - 1).toString();
-		return st;
+		return userService.getUsuariosByName(name);
 	}
 	
 	@ApiOperation(value="Create a User")
 	@PostMapping("/")
 	public User postUser(@RequestBody @Valid User user) {
-		return userRepository.save(user);
+		return userService.postUser(user);
 	}
 
 	@ApiOperation(value="Delete a User")
 	@DeleteMapping({"/{id}/", "/{id}"})
-	public void deleteUser(@PathVariable(value="id") String id) {
-		User user =  userRepository.findById(id);
-		userRepository.delete(user);
+	public ResponseEntity<?> deleteUser(@PathVariable(value="id") String id) {
+		userService.deleteUser(id);
+		
+		return ResponseEntity.ok().build();
+		
 	}
 	
 	@ApiOperation(value="Update a User")
 	@PutMapping({"/{id}/", "/{id}"})
 	public User atualizaUsuario(@PathVariable(value="id") String id, @RequestBody @Valid User user) {
-		User existingUser =  userRepository.findById(id);
 		
-		if (!(user.getName() == null) && !(user.getName().trim().equals(""))) {
-			existingUser.setName(user.getName());
-		}
-		
-		if (!(user.getEmail() == null) && !(user.getEmail().trim().equals(""))) {
-			existingUser.setEmail(user.getEmail());
-		}
-		
-		if (!(user.getTelephone() == null) && !(user.getTelephone().trim().equals(""))) {
-			existingUser.setTelephone(user.getTelephone());
-		}
-		return userRepository.save(existingUser);
+		return userService.atualizaUsuario(id, user);
 	}
 
 
--- src/main/java/com/e/doe/manager/user/UserService.java
+++ src/main/java/com/e/doe/manager/user/UserService.java
@@ -1,5 +1,61 @@
 package com.e.doe.manager.user;
 
-public class UserService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> getUsers(){
+		return userRepository.findAll();
+	}
+	
+	public User getUserById(String id){
+		return userRepository.findById(id);
+	}
+	
+	public String getUsuariosByName(String name){
+
+		List<User> users = userRepository.findByName(name);
+		String st = "";
+		
+		for (int i = 0; i < users.size() - 1; i++) {
+			st += users.get(i).toString() + " | ";
+		}
+
+		st += users.get(users.size() - 1).toString();
+		return st;
+	}
+	
+	public User postUser(User user) {
+		return userRepository.save(user);
+	}
+
+	public void deleteUser(String id) {
+		User user =  userRepository.findById(id);
+		userRepository.delete(user);
+	}
+	
+	public User atualizaUsuario(String id, User user) {
+		User existingUser =  userRepository.findById(id);
+		
+		if (!(user.getName() == null) && !(user.getName().trim().equals(""))) {
+			existingUser.setName(user.getName());
+		}
+		
+		if (!(user.getEmail() == null) && !(user.getEmail().trim().equals(""))) {
+			existingUser.setEmail(user.getEmail());
+		}
+		
+		if (!(user.getTelephone() == null) && !(user.getTelephone().trim().equals(""))) {
+			existingUser.setTelephone(user.getTelephone());
+		}
+		return userRepository.save(existingUser);
+	}
 }
