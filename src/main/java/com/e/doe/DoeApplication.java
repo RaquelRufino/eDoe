976a941c155e22eb2afeac7e2bebc2cd2fd9a882
RaquelRufino <quelrufino35@gmail.com>
2019-10-19 18:54:59 -0300

    add cache

--- src/main/java/com/e/doe/DoeApplication.java
+++ src/main/java/com/e/doe/DoeApplication.java
@@ -3,8 +3,10 @@ package com.e.doe;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class DoeApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/e/doe/manager/item/ItemService.java
+++ src/main/java/com/e/doe/manager/item/ItemService.java
@@ -2,7 +2,11 @@ package com.e.doe.manager.item;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
 
 
@@ -13,30 +17,32 @@ public class ItemService {
 	@Autowired
 	private ItemRepository itemRepository;
 	
-	
+    @Cacheable(cacheNames = Item.CACHE_NAME, key="#root.method.name")
 	public List<Item> getItems(){
 		
 		List<Item> items = itemRepository.findAll();
 		
 		return items;
 	}
-	
+    
+    @Cacheable(cacheNames = Item.CACHE_NAME, key="#identifier")
 	public Item getItem(long id){
 		return itemRepository.findById(id);
 	}
 	
-	
+    @CacheEvict(cacheNames = Item.CACHE_NAME, allEntries = true)
 	public Item postItem(Item item) {
 
 		return itemRepository.save(item);
 	}
 
-	
+    @CacheEvict(cacheNames = Item.CACHE_NAME, key="#identifier")
 	public void deleteItem(long id) {
 		Item item = itemRepository.findById(id);
 		itemRepository.delete(item);
 	}
 	
+    @CachePut(cacheNames = Item.CACHE_NAME, key="#company.getIdentifier()")
 	public Item updateItem(long id, Item item) {
 		
 		Item existingItem =  itemRepository.findById(id);
